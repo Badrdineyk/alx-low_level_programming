0e24fcb49816a570c8ea54c9db1ba93dbbfc3976
Badrdineyk <badrdineyk9@gmail.com>
2023-07-13 08:06:53 -0700

    Add: 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - Prints "Programming is like building a multilingual 
+ * 		  puzzle, followed by a new line.
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
