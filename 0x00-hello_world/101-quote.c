3e10c15691fe1339d926593cac51a419a60953f7
Badrdineyk <badrdineyk9@gmail.com>
2023-07-13 11:53:04 -0700

    Add: 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,23 +3,17 @@
  * Auth: Badreddine Toumani
  */
 
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints and that piece of art is useful"
- * - Dora Korpar, 2015-10-19
+ * main - Prints "and that piece of art is useful" - Dora korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  *
- *   Return: 1.
+ * Return: Always 1.
  */
-
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	while (*s)
-	{
-		putchar(*s++);
-	}
+	write(2, "and that piece of art is useful\" - Dora kprpar, 2015-10-19\n", 59);
 
 	return (1);
 }
