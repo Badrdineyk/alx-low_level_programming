ede3c0995b211670bbb4bbba3250455934851643
Badrdineyk <badrdineyk9@gmail.com>
2023-07-14 09:47:16 -0700

    Add: 4-print_alphabt. file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: Badreddine Toumani
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase except q qnd e,
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
