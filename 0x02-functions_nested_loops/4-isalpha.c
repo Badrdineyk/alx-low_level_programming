e70604efa1e6328a7534aefed90a3cd87e6041ed
Badrdineyk <badrdineyk9@gmail.com>
2023-07-18 07:42:05 -0700

    Add: 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Badreddine Toumani
+ */
+
+#include "main.h"
+
+/**
+ * -isalpha - Checks if a character is lowercase or uppercase.
+ *  @c: The character to be checked.
+ *
+ *  Return: 1 if character is lowercase or uppercase, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
