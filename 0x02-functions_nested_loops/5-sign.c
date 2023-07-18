bbb8cd62d18d595aada0a945a17c8e074742182e
Badrdineyk <badrdineyk9@gmail.com>
2023-07-18 08:09:09 -0700

    Clean the code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * print-sign - Prints the sign of a number.
+ * print_sign - Prints the sign of a number.
  * @n: The number to be checked
  *
  * Return: 1 if the number is greater than zero,
