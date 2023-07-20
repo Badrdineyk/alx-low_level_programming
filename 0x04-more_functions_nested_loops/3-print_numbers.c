faa1de108e94bddf895da7004ada2f85104910c0
Badrdineyk <badrdineyk9@gmail.com>
2023-07-20 05:14:24 -0700

    Add: 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Badreddine Toumani
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9,
+ * followed by a new line.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
