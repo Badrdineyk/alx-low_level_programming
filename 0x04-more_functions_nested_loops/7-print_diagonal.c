e2e978608b412340e007d70674c663598d0d1f6b
Badrdineyk <badrdineyk9@gmail.com>
2023-07-20 14:04:27 -0700

    Add: 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -23,12 +23,12 @@ void print_diagonal(int n)
 				_putchar(' ');
 			}
 			_putchar('\\');
+
+			if (i == n - 1)
+				continue;
+
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
