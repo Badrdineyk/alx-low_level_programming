34a35fb0b6b13b9cc4ee1bfe037cbc42c807c63a
Badrdineyk <badrdineyk9@gmail.com>
2023-07-24 11:21:35 -0700

    Add: 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-puts2.c
+ * Auth: Badreddine Toumani
+ */
+
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string,
+ * starting with the first character.
+ * @str: The string containing characters.
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int index = 0;
+
+	while (str[index++])
+		length++;
+
+	for (index = 0; index < length; index =+ 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
