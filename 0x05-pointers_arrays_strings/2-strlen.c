63354a0d451a91712136df1084e10e516c635048
Badrdineyk <badrdineyk9@gmail.com>
2023-07-24 08:10:23 -0700

    Add: 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Badreddine Toumani
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of a string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+
+		*s++;
+	}
+
+	return (length);
+}
