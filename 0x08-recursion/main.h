2a0d5d8a3eff9665f1ca5576861921b053f945a9
Badrdineyk <badrdineyk9@gmail.com>
2023-08-02 03:11:05 -0700

    Add: main.h file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Badreddine Toumani
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x08-recursion directory.
+ */
+
+int _putchar(char c);
+
+#endif
