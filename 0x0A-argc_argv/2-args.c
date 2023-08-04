931553d544a54ad3c8de0bf3e1f9529355e413f5
Badrdineyk <badrdineyk9@gmail.com>
2023-08-04 05:51:10 -0700

    Add: 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-args.c
+ * Auth: Badreddine Toumani
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int index;
+
+	for (index = 0; index < argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+
+	return (0);
+}
