031cccdb315000611729df681499c68c09e99e63
Badrdineyk <badrdineyk9@gmail.com>
2023-08-14 07:49:30 -0700

    Add: dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The dog's owner.
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
