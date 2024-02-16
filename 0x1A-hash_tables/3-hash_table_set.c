5d3da14b269399bdde888aadfdc392d6c05e2503
Badrdineyk <badrdineyk9@gmail.com>
2024-02-16 07:30:24 -0800

    Task 3 is solved

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: The key
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index, i;
+	char *cpy_value;
+
+	cpy_value = strdup(value);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	cpy_value = strdup(value);
+	if (cpy_value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = cpy_value;
+			return (1);
+		}
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+	{
+		free(cpy_value);
+		return (0);
+	}
+	node->key = strdup(key);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+
+	return (1);
+}
